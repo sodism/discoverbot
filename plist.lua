1182aa9025167bbfaa82dfca85de5ac364fe23e0
sodism <s.sm21@rayana.ir>
2016-05-10 21:05:34 +0430

    Add files via upload

---
+++ plist.lua
@@ -0,0 +1,22 @@
+do
+
+function run(msg, matches)
+  return [[http://www.8up.ir/images/kkvm02ko5jvj13t4oxr.jpg]]
+end
+return {
+  description = ".", 
+  usage = " ",
+  patterns = {
+    "^/planlist$",
+    "^!planlist$",
+    "^planlist$",
+    "^$planlist$",
+   "^/planlist$",
+   "^planlist",
+   "^planlist$",
+   "^#planlist$",
+
+  },
+  run = run
+}
+end
