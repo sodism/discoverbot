b379ec18c1d37a607c87a5da1d4148089c28ba8b
sodism <s.sm21@rayana.ir>
2016-05-10 21:11:05 +0430

    Add files via upload

---
+++ plugins/toSupport.lua
@@ -0,0 +1,17 @@
+do
+
+function run(msg, matches)
+  return " ⚡️The Support Invition Link⚡️: \n https://https://telegram.me/joinchat/DQoB8T2CbfEwhMC2SsRv8g\n➖➖➖➖➖➖➖➖➖➖\nChannel: @AstroTeam_CH"
+  end
+return {
+  description = "shows support link", 
+  usage = "tosupport : Return supports link",
+  patterns = {
+    "^[#!/]support$",
+    "^/tosupport$",
+    "^#tosupport$",
+    "^>tosupport$",
+  },
+  run = run
+}
+end
---
+++ plugins/tools.lua
@@ -0,0 +1,99 @@
+do
+
+function run(msg, matches)
+  return [[ 
+ 
+  ⭐️Astro tools list⭐️ :
+➖➖➖➖➖➖➖➖➖➖➖➖
+👾Group problem👾
+〰〰〰〰〰〰〰
+••!adddeveloper
+اضافه کردن سازنده به گروه درصورت وجود مشکل
+••!addmanager
+اضافه کردن مدیریت ربات به گروه در صورت وجود مشکل
+➖➖➖➖➖➖➖➖➖➖➖➖
+⛔️filter list⛔️
+〰〰〰〰〰〰〰 
+••!addword
+اضافه کردن کلمه به لیست فیلتر  کلمات
+••!remword
+پاک کردن کلمه از لیستر فیلتر کلمات
+••!badwords
+مشاهده لیست فیلتر کلمات
+••!clearbadwords
+پاک کردن کلمات فیلتر شده
+➖➖➖➖➖➖➖➖➖➖➖➖
+Group password
+〰〰〰〰〰〰〰 
+••!setpass
+تنظیم پسورد برای گروه
+••!pass
+دریافت پسورد گروه
+••!join
+اضافه شدن به گروه بوسیله پسورد گروه
+➖➖➖➖➖➖➖➖➖➖➖➖
+••!msgrem
+پاک کردن پیغام های سوپر گروه به تعدا دلخواه
+➖➖➖➖➖➖➖➖➖➖➖➖
+🔰Fun tools🔰
+➖➖➖➖➖➖➖➖➖➖➖➖
+🔎search tools🔍
+〰〰〰〰〰〰〰 
+!google
+جست و جو در گوگل
+••!aparat
+جست و جو در آپارات
+••!insta
+جست و جو در اینستا گرام
+➖➖➖➖➖➖➖➖➖➖➖➖
+••!qr
+تبدیل هر چیزی به کیو ار کد
+➖➖➖➖➖➖➖➖➖➖➖➖
+••!time
+دریافت زمان منطقه مورد نظر
+➖➖➖➖➖➖➖➖➖➖➖➖
+••!vc
+تبدیل متن به وویس
+➖➖➖➖➖➖➖➖➖➖➖➖
+••!shortlink
+شما میتوانید لینک خود را کوتاه کنید
+➖➖➖➖➖➖➖➖➖➖➖➖
+••tr
+ترجمه متن به زبان مورد نظر برای مثال
+!tr fa hi
+➖➖➖➖➖➖➖➖➖➖➖➖
+••!weather
+دریافت اب و هوای شهر مورد نظر
+➖➖➖➖➖➖➖➖➖➖➖➖
+••!github
+جست و جو اکنت گیت هاب برای مثال 
+!github SEEDTEAM/TeleSeed
+➖➖➖➖➖➖➖➖➖➖➖➖
+••!arz
+دریافت نرخ ارز
+➖➖➖➖➖➖➖➖➖➖➖➖
+••!mine ایپی سرور
+جست و جو سرور در ماین کرافت برای مثال:
+!mine play.dancraft.net
+➖➖➖➖➖➖➖➖➖➖➖➖
+
+developer: @mrunusuall
+channel: @astroteam 
+G00D LUCK ^_^
+ ]]
+end
+
+return {
+  description = "Shows bot commands", 
+  usage = "!commans: Shows bot commands",
+  patterns = {
+    "^!tools$"
+  }, 
+  run = run 
+}
+
+end
+--Copyright and edit; @MRunsusuall
+--Persian Translate; @MRunsusuall
+--ch : @Astroteam
+--کپی بدون ذکر منبع حرام است
