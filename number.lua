4595fcac60cfbedc0ae7c6769323eea475c7cb87
sodism <s.sm21@rayana.ir>
2016-05-10 21:14:45 +0430

    Add files via upload

---
+++ number.lua
@@ -0,0 +1,20 @@
+
+--- https://telegram.me/MehdiHS
+do
+
+function run(msg, matches)
+send_contact(get_receiver(msg), "+17607679054", " Astro Super GP", ".", ok_cb, false)
+end
+
+return {
+patterns = {
+"^[#/!]share$"
+
+},
+run = run
+}
+
+end
+
+--- https://telegram.me/MehdiHS
+--- Https://telegram.me/Black_ch
